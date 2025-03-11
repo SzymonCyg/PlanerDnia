ba7579743839445abc856c7b1736f6e49d48c3ed
szymon <sda@sda/pl>
2025-03-11 08:59:51 +0100

    Ukonczenie pierwszego stack panela

--- Planer/obj/Debug/net9.0/Planer.AssemblyInfo.cs
+++ Planer/obj/Debug/net9.0/Planer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Planer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf621bae76b66214d71f37179f6d42680a58ffcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Planer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Planer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
