0b8cc13dae325933c2d734b0e66f58d0b7b1340b
szymon <sda@sda/pl>
2025-03-18 08:42:16 +0100

    Dodanie grida i kolejnego stack panela

--- Planer/MainWindow.axaml.cs
+++ Planer/MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace Planer;
 
@@ -7,5 +9,22 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        SubmitButton.Click += SubmitButton_Click;
+    }
+    private void SubmitButton_Click(object? sender, RoutedEventArgs e)
+    {
+        try
+        {
+            var checkBoxValue = CheckBox.IsChecked == true ? "Ukończono zadanie" : "Nie ukończono zadania";
+            var textBoxValue = TextBox.Text;
+            var 
+
+        }
+        catch (Exception ex)
+        {
+            Podsumowanie.Text = ex.Message;
+        }
+        
+      
     }
 }
